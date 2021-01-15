e19ba00e484fcf29949c6ac9499ee0e42c14360e
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-01-15 18:20:55 +0530

    commit

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,13 @@
+public class fibonacci {
+    public static int fib(int n){
+        if(n==0)
+        return 0 ;
+        if(n==1)
+        return 1;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        int n =5 ;
+        System.out.println(fib(n));
+    }
+}
