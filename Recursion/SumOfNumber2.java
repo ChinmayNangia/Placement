bc1445a1a96cdf0411da88c458376f874e12d00e
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-01-16 01:52:36 +0530

    optimizing

--- Recursion/SumOfNumber2.java
+++ Recursion/SumOfNumber2.java
@@ -1,11 +1,11 @@
 public class SumOfNumber2 {
     public static int sum(int n){
-        if(n==0)
-        return  0 ;
+        if(n<=9)
+        return  n ;
          return sum(n/10)+n%10;
     }
     public static void main(String[] args) {
-        int  n =253;
+        int  n =225;
         System.out.println(sum(n));
     }
 }
