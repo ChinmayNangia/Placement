f01c1e02c22e68e39b802cf34791bb543874a8be
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-01-17 15:57:59 +0530

    commited today

---
+++ Recursion/josephus.java
@@ -0,0 +1,16 @@
+public class josephus{
+    public static int jos(int n,int k){
+        if(n==1)
+        return 0;
+        else
+        return (jos(n-1, k)+k)%n;
+    }
+    public static void main(String[] args) {
+        int n = 7 ;
+        int k =3;
+        System.out.println("normal solution");
+        System.out.println(jos(n, k));
+        System.out.println("extended problem where  it starts from 1");
+        System.out.println(jos(n, k)+1);
+    }
+}
