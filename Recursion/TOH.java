fd972eed8cd376b74d334bb796cf30ee48bac315
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-01-19 00:40:14 +0530

    updated TOH

--- Recursion/TOH.java
+++ Recursion/TOH.java
@@ -1,11 +1,11 @@
 public class TOH {
     public static void tower(int n , char a ,char b ,char c){
       if(n==1){
-          System.out.println("move 1 "+ " from " +a+" to "+" c");
-          return;
+        System.out.println("Move 1 from " +  a + " to " + c); 
+        return;
       }
       tower(n-1, a, c, b);
-      System.out.println("move 1 "+" from"+ a+" to "+c);
+      System.out.println("Move " + n + " from " +  a + " to " + c); 
       tower(n-1, b, a, c);
 
 
