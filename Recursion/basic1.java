934d8cff144480a4cf1f191db7ba7acd2004dccd
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-01-24 14:50:07 +0530

    update

--- Recursion/basic1.java
+++
@@ -1,9 +0,0 @@
-public class basic1 {
-    public static void fun1(){
-        System.out.println("GFG");
-        fun1();
-    }
-    public static void main(String[] args) {
-     fun1();   
-    }
-}
