561deb31476b3e53e593e488b13ed6cd33975cf6
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-02-03 23:32:47 +0530

    update

---
+++ Array/MaxOccurence.java
@@ -0,0 +1,22 @@
+public class MaxOccurence {
+    public static int strong(int L[],int R[],int n){
+        int arr[] = new int[1000];
+        for(int i = 0 ;i <n;i++)
+        {
+            arr[L[i]]++;
+            arr[R[i]-i]--;
+        }
+        int maxm = arr[0] , res = 0 ;
+        for(int i = 0 ;i<n;i++){
+            arr[i]+= arr[i-1];
+            if(arr[i]> maxm){
+                maxm = arr[i];
+                res = i ;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
