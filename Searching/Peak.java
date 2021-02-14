e2331e9919542ce5985630fff94448dafdb45ed6
Chinmay Nangia <63157121+thekillingjoke19@users.noreply.github.com>
2021-02-14 13:24:25 +0530

    Update Peak.java

--- Searching/Peak.java
+++ Searching/Peak.java
@@ -16,7 +16,7 @@ public class Peak {
         int mid = (low+high)/2;
         if(mid == 0 || arr[mid -1]<=arr[mid] && mid==n-1 || arr[mid+1]<=arr[mid])
             return arr[mid];
-        else if(mid>=0|| arr[mid]>=arr[mid-1])
+		else if(mid > 0 && arr[mid - 1] >= arr[mid])
             high = mid-1;
         else
             low = mid+1;    
