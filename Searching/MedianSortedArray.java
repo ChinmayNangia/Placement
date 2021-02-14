3f97024466693a460fb3b794e87c1436fe122b0d
Chinmay Nangia <63157121+thekillingjoke19@users.noreply.github.com>
2021-02-15 02:31:34 +0530

    Update MedianSortedArray.java

--- Searching/MedianSortedArray.java
+++ Searching/MedianSortedArray.java
@@ -6,17 +6,18 @@ public class MedianSortedArray {
     int y = input2.length;
     
     int low = 0 ;
-    int high = x-1;
+    int high = x;
     while(low<=high){
         int partitionX = (low+high)/2;
         int partitionY = (x+y+1)/2 - partitionX;
 
-        int maxLeftX = (partitionX==0)? Integer.MIN_VALUE:input1[partitionX-1];
-        int minRightX = (partitionX==x)?Integer.MAX_VALUE:input1[partitionX];
+       int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
 
-        int maxLeftY = (partitionY==0)?Integer.MIN_VALUE:input2[partitionY-1];
-        int minRightY = (partitionY==y)?Integer.MAX_VALUE:input2[partitionY];
-        if(maxLeftX<=minRightX && maxLeftY<=minRightY){
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
             if(x+y%2==0)
                 return ((double) Math.max(maxLeftX, maxLeftY)+ Math.min(minRightX, minRightY)/2);
             else
