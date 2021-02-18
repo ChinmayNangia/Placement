2c0dd243007d4788b32e3d68d2c81dce82b67e94
thekillingjoke19 <chinmaynangia52@gmail.com>
2021-02-18 15:05:25 +0530

    new

---
+++ Sorting/Arrays.sort/Example2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int[] arr =  {5, 30, 20, 10, 8}; 
+        Arrays.sort(arr,1,3);
+        System.out.println(Arrays.toString(arr));
+     }
+}
